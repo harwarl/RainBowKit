ee147490e482f6f580d666406419b99c6e6ad7e9
harwarl <harwarl87@gmail.com>
2023-05-19 18:16:37 +0100

    feat: can Connect wallets using rainbow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+        "net": false,
+        "tls": false
+      }
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
 import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig, Chain} from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(  ///sets up the chains
+  [mainnet],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({  //get connectors to create a wagmi client
+  appName: "TestApp",
+  projectId: "5c69e64be9a09cb0d7b93d6ae5f09b40",
+  chains
+})
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />  
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
